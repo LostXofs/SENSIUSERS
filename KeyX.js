86b836fc4e3b8b0c516e001f3fbe198691f3f945
LostXofs <lostmmma@gmail.com>
2025-01-17 05:48:58 +0100

    Create KeyX.js

---
+++ KeyX.js
@@ -0,0 +1,12 @@
+loopy() 
+ function loopy() { 
+ var sWord ="Tele : @LostXofs" 
+ while (sWord != "Jenn") {// Pass
+ sWord = prompt("Enter The Key :")  
+ } 
+ 
+//confirm("ok");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 SENSI_USERS ( LIFE TIME ) ");
+
+ }
