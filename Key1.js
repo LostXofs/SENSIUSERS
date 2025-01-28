17a2d974c0c26ac39c15b882f45050bb158b0651
LostXofs <lostmmma@gmail.com>
2025-01-28 01:13:32 +0100

    Create Key1.js

---
+++ Key1.js
@@ -0,0 +1,12 @@
+loopy() 
+ function loopy() { 
+ var sWord ="Tele : @LostXofs" 
+ while (sWord != "JEAN") {// Pass
+ sWord = prompt("Enter The Key :")  
+ } 
+ 
+//confirm("ok");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 𝐉𝐄𝐀𝐍 𝐂𝐇𝐄𝐀𝐓 ");
+
+ }
