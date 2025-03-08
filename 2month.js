b8f254d7db626b9dce6a2ca629f57bfb03c51092
LostXofs <lostmmma@gmail.com>
2025-03-08 01:46:36 +0100

    Create 2month.js

---
+++ 2month.js
@@ -0,0 +1,11 @@
+
+loopy() 
+ function loopy() { 
+ var sWord ="Tele : @LostXofs" 
+ while (sWord != "M3SB DANGER") {// Pass
+ sWord = prompt("Enter The Key :")  
+ } 
+ 
+//confirm("ok");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 ");
