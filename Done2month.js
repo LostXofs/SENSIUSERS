83a880f362b2acb4d0a71f801728392000447837
LostXofs <lostmmma@gmail.com>
2025-03-08 02:39:21 +0100

    Create Done2month.js

---
+++ Done2month.js
@@ -0,0 +1,12 @@
+loopy() 
+ function loopy() { 
+ var sWord ="@LostXofs" 
+ while (sWord != "M3SB") {// Pass
+ sWord = prompt("Enter The Uesr ")  
+ } 
+ 
+//confirm("ok");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋  ");
+
+}
